56c46f9bd7ba565f168bac233aad4f3eecb2d00f
Derek <derekduranizk@gmail.com>
2020-05-15 22:25:41 -0500

    Test 2.0

---
+++ API REST Northwind/App_Start/WebApiConfig.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using System.Web.Cors;
+
+namespace API_REST_Northwind
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de API web
+            var formatters = GlobalConfiguration.Configuration.Formatters;
+            var jsonFormatter = formatters.JsonFormatter;
+            var settings = jsonFormatter.SerializerSettings;
+
+            jsonFormatter.SerializerSettings.PreserveReferencesHandling = PreserveReferencesHandling.None;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+            settings.Formatting = Formatting.Indented;
+            settings.NullValueHandling = NullValueHandling.Ignore;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            config.EnableCors();
+            // Rutas de API web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ API REST Northwind/Controllers/NorthwindController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using System.Web.Http.Cors;
+
+namespace API_REST_Northwind.Controllers
+{
+    [EnableCors(origins:"*",headers:"*",methods:"*")]
+    [RoutePrefix("Northwind")]
+    public class NorthwindController : ApiController
+    {
+        [HttpGet]
+        [Route("Test")]
+        public HttpResponseMessage Testing()
+        {
+            return Request.CreateResponse(
+                HttpStatusCode.OK,
+                "Test exitoso");
+        }
+    }
+}
